27f50b5acf1f7c267aeb19393a7c96801efbe0db
Norman <33242949+nbfajanilan@users.noreply.github.com>
2019-04-13 14:56:20 +0800

    Week 2

--- caesar.c
+++ caesar.c
@@ -6,7 +6,7 @@
 void key_check(string s);
 void input_key(int s);
 string cipher(string t, int n);
-    
+
 int main(int argc, string argv[])
 {
     if (argc != 2)
@@ -23,11 +23,12 @@ int main(int argc, string argv[])
     printf("ciphertext: %s\n", ctext);
 }
 
+// check if inout key is valid
 void key_check(string s)
 {
-    for (int i=1, l = strlen(s); i < l; i++ )
+    for (int i = 1, l = strlen(s); i < l; i++)
     {
-        if (s[i]<'0' || s[i]>'9')
+        if (s[i] < '0' || s[i] > '9')
         {
             printf("Usage: ./caesar key\n");
             exit(1);
@@ -44,28 +45,31 @@ void input_key(int k)
     if (k < 1)
     {
         printf("Usage: ./caesar key\n");
-        exit(0);
+        exit(1);
     }
 }
 
+//function to encrypt the text
 string cipher(string t, int n)
 {
     int l = strlen(t);
-    for (int i=0; i < l; i++)
+    for (int i = 0; i < l; i++)
     {
-        int s = t[i]+n;
+        int s = t[i] + n;
         if (t[i] < 'A' || (t[i] > 'Z' & t[i] < 'a') || t[i] > 'z')
         {
             continue;
         }
         else if (t[i] <= 'z' & s > 'z') 
         {
-            int d = s -'z'; 
+            int d = s - 'z';
+            d = d % 26;
             t[i] = 'a' + d - 1;  
         }
         else if (t[i] <= 'Z' & s > 'Z')
         {
-            int d = s -'Z'; 
+            int d = s - 'Z'; 
+            d = d % 26;
             t[i] = 'A' + d - 1;  
         }
         else
